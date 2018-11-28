d861347b054538062251da4e953fd68ee212b3e2
Miios <nathan.anne@epitech.eu>
2018-11-28 19:37:13 +0100

    add script for enigma on stage 1-2

---
+++ The Escape Vr/Assets/Level1/Assets/Scripts/lamp_allume.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lamp_allume : MonoBehaviour {
+
+	public BoxCollider Box1;
+	public SphereCollider LeftHands;
+	public SphereCollider RightHands;
+	// Use this for initialization
+	public Light[] OfficeLight;
+	public GameObject Paper;
+	void OnTriggerEnter(Collider col) {
+		if (col == LeftHands || col == RightHands) {
+			for (int i = 0; i < OfficeLight.Length; i++)
+				OfficeLight[i].enabled = true;
+			Paper.SetActive(true);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
