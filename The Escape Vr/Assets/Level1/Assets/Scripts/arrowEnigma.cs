c49989b4d43aed6418580ed1ca052f6248c195dd
Max Brooks <max.brooks83@gmail.com>
2018-11-29 12:23:07 +0100

    Script enigma arrow

---
+++ The Escape Vr/Assets/Level1/Assets/Scripts/arrowEnigma.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowEnigma : MonoBehaviour {
+	public TextMesh textHolder;
+	public Collider handLeft;
+	public Collider handRight;
+	
+	void OnTriggerEnter(Collider col) {
+		if (col == handLeft || col == handRight) {
+			if (this.tag == "colliderLeft")
+				textHolder.text += "< ";
+			else if (this.tag == "colliderRight")
+				textHolder.text += "> ";
+			else
+				textHolder.text = "";
+		}
+	}
+}
